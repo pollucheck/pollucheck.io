2c705a6b967c91a21d013adaa17770157cf48529
yuvaraj-06 <59510075+yuvaraj-06@users.noreply.github.com>
2020-03-15 13:52:45 +0530

    KK

---
+++ APPLICATION/Assets/Scenes/BUTTONS.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BUTTONS : MonoBehaviour
+{
+    public void mainPage()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void payment()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void AAA()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void AAB()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void AAC()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void BACK()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ APPLICATION/Assets/Scenes/Class1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Class1 : MonoBehaviour
+{
+    public void URL()
+    {
+        Application.OpenURL("https://answers.unity.com/questions/1245858/use-a-button-to-open-a-web-browser.html");
+        Debug.Log("is this working?");
+    }
+    public void URL1()
+    {
+        Application.OpenURL("https://www.icicibank.com/online-services/scan-to-pay/cards-payment.page");
+        Debug.Log("is this working?");
+    }
+    public void URL2()
+    {
+        Application.OpenURL("https://www.bhimupi.org.in/");
+        Debug.Log("is this working?");
+    }
+    public void URL3()
+    {
+        Application.OpenURL("https://paytm.com/");
+        Debug.Log("is this working?");
+    }
+
+}
